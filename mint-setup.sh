8daae6a4a4aed7a5d5e61008c927afb7704b71d0
niemisami <sami@nieminen.fi>
2017-11-12 18:54:57 +0200

    update/upgrade system and install basic packages

--- mint-setup.sh
+++ mint-setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Exit every time if something goes wrong at any point
 set -e
 
 echo 'Will install everything for you in the near future (not a promise..)'
+
+# Basic uUpdate and upgrade
+#apt-get update
+#apt-get upgrade
+
+# Basic packages
+PACKAGES='build-essential libssl-dev ufw'
+apt-get install -y $PACKAGES
+
+
+# Prompt OK if last command was successful
+if (( $? )) ; then echo failed ; else echo OK; fi
