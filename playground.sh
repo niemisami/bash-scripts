0ee2f0eb757e19b5572b85ece8d6ed35434f9ea0
niemisami <sami@nieminen.fi>
2017-11-12 22:02:31 +0200

    playing around with scripts

--- playground.sh
+++ playground.sh
@@ -1,6 +1,34 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
 echo Playground scripts 
 
+ARRAY=('java', 'python', 'C', 'elm');
+
+# Print whole array
+echo ${ARRAY[@]}
+
+# User input 
+
+if [[ $1 = -p ]]; then
+	read -p "What your name: " ANSWER
+	echo "Your name is: $ANSWER"
+fi
+
+# Calculate script parameters
+echo $#
+
+# List folder files
+echo $PWD
+for SCRIPT in $PWD/*
+do
+if [ -f $SCRIPT ] 
+then 
+echo $SCRIPT
+fi 
+done
+
+
+
+if (( $? )) ; then echo failed ; else echo OK; fi
