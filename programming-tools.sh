a32a713e7b02cb58c937e77e0af87a22b9a0429a
niemisami <sami@nieminen.fi>
2017-11-19 10:41:00 +0200

    install vs code, spotify

---
+++ programming-tools.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# Visual Studio Code
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+# SQLite browser
+
+
+apt-get update
+apt-get install code
---
+++ python-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e 
+
+# Python3 and pip
+apt-get install -y python3-pip
+apt-get install -y python3-venv
+
