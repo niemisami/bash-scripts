25ef442614ec0b44acd834918a07d29adf04dabd
niemisami <sami@nieminen.fi>
2018-02-19 11:48:02 +0200

    added handy log git aliases

--- setup-git.sh
+++ setup-git.sh
@@ -10,6 +10,18 @@ echo 'Setup common aliases'
 git config --global alias.cm 'commit -m'
 git config --global alias.st 'status'
 git config --global alias.amend 'commit --no-edit --amend'
+git config --global alias.lg "log --graph --pretty=format:'%C(auto)%h %C(cyan)%cr | %C(auto)%d%Creset%s %C(blue ul bold)%an%Creset' --date=relative"
+# Show details about last commit
+git config --global alias.lgl 'log -p -1'
+
+## Userful scripts to add in .gitconfig
+# Search commits by name
+# !git log --pretty=format:'%C(auto)%h %C(cyan)%cr | %C(auto)%d%Creset%s %C(blue ul bold)%an%Creset' --date=relative | grep
+
+# Get commits n days before today
+#!f() { \
+#   git log --after=\"$(date -j -v-$1d +%Y-%m-%d)\" --oneline; \
+#}; f
 #git config --global alias.
 
 # Push only the current or specified branch 
